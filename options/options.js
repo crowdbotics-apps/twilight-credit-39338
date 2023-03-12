d9fc72111a99291a6776886d1fe0869ba8da3cd7
Crowdbotics <team@crowdbotics.com>
2023-03-12 08:16:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-credit-39338",
+  url: "https://twilight-credit-39338.botics.co",
+  api: "https://twilight-credit-39338.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
